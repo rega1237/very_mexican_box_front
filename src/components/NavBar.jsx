1cf1b856464b232d130f11068ad84392a2e81eb8
rega1237 <rega1237@gmail.com>
2023-12-07 15:56:45 -0400

    add the navlink to signup

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -46,7 +46,7 @@ const NavBar = ({removeNavBar, addNavBar}) => {
 			<li><a className="text-sm text-gray-400 hover:text-pink" href="#">Contacto</a></li>
 		</ul>
 		<NavLink to="/signin" onClick={removeNavBar} className="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-pink hover:text-white text-sm text-gray-900 font-bold  rounded-xl transition duration-200" href="#">Iniciar Sesión</NavLink>
-		<a className="hidden lg:inline-block py-2 px-6 bg-pink hover:bg-white hover:text-pink text-sm text-white font-bold rounded-xl transition duration-200" href="#">Registrarme</a>
+		<NavLink to="/signup" onClick={removeNavBar} className="hidden lg:inline-block py-2 px-6 bg-pink hover:bg-white hover:text-pink text-sm text-white font-bold rounded-xl transition duration-200" href="#">Registrarme</NavLink>
 	</nav>
 	<div className={`navbar-menu relative z-50 ${isOpen ? "" : "hidden"}`}>
 		<div className="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25" onClick={handleToggleOpen}></div>
@@ -80,7 +80,7 @@ const NavBar = ({removeNavBar, addNavBar}) => {
 			<div className="mt-auto">
 				<div className="pt-6">
 					<NavLink to="/signin" onClick={removeNavBar} className="block px-4 py-3 mb-3 leading-loose text-xs text-center font-semibold leading-none text-pink bg-gray-50 hover:bg-gray-100 rounded-xl" href="#">INICIAR SESIÓN</NavLink>
-					<a className="block px-4 py-3 mb-2 leading-loose text-xs text-center text-white font-semibold bg-pink hover:bg-blue-700  rounded-xl" href="#">REGISTRARME</a>
+					<NavLink to="/signup" onClick={removeNavBar} className="block px-4 py-3 mb-2 leading-loose text-xs text-center text-white font-semibold bg-pink hover:bg-blue-700  rounded-xl" href="#">REGISTRARME</NavLink>
 				</div>
 				<p className="my-4 text-xs text-center text-gray-400">
 					<span>Copyright © 2023</span>
