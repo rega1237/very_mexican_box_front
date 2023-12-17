08e64ebf99b4754568ea60480c064bdcea210fff
rega1237 <rega1237@gmail.com>
2023-12-17 13:43:35 -0400

    add state to setPlan and create function to hadle error modal at close

--- src/pages/subscription/Subscription.jsx
+++ src/pages/subscription/Subscription.jsx
@@ -1,21 +1,31 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import UserContext from "../../components/store/userContext";
 import TitleSub from "./TittleSub";
 import AllCards from "./AllCards";
 import Form from "./form/CompleteFormModal"
 
 const Subscription = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [plan, setPlan] = useState(1)
+
+  const userCtx = useContext(UserContext);
 
   const handleModal = () => {
     setModalIsOpen(!modalIsOpen)
+    userCtx.setError(false, "");
+  }
+
+  const handleModalPlan = (id) => {
+    setPlan(id)
+    handleModal()
   }
   
   return (
     <section className="bg-black text-white pb-9">
-      <Form modalIsOpen={modalIsOpen} handleModal={handleModal} />
+      <Form modalIsOpen={modalIsOpen} handleModal={handleModal} plan={plan} />
       <TitleSub />
       <div>
-        <AllCards handleModal={handleModal} />
+        <AllCards handleModalPlan={handleModalPlan}  />
       </div>
     </section>
   )
