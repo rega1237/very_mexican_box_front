6af80eed24c7a91cdeae2cff27e5fe0518d11924
rega1237 <rega1237@gmail.com>
2023-12-19 12:42:55 -0400

    add logout button to navbar

--- src/components/NavBar/LoggedComponentMobile.jsx
+++ src/components/NavBar/LoggedComponentMobile.jsx
@@ -1,9 +1,18 @@
 import { NavLink } from 'react-router-dom';
+import { useContext } from 'react';
+import UserContext from '../store/userContext';
 
 const LoggedComponentMobile = () => {
+  const userCtx = useContext(UserContext);
+
+  const logOutUser = () => {
+    userCtx.logOut();
+  }
+
   return (
-    <div className='w-full'>
-      <NavLink to="/" className="block lg:hidden w-full px-4 py-3 mb-2 leading-loose text-xs text-center text-white font-bold bg-pink hover:bg-gray-50 hover:text-pink  rounded-xl" href="#">Mi Cuenta</NavLink>
+    <div className='flex flex-col w-full lg:hidden'>
+      <NavLink to="/" className="block w-full px-4 py-3 mb-2 leading-loose text-xs text-center text-white font-bold bg-pink hover:bg-gray-50 hover:text-pink  rounded-xl" href="#">Mi Cuenta</NavLink>
+      <button onClick={logOutUser}>Cerrar Sesión</button>
     </div>
     
   ) 
