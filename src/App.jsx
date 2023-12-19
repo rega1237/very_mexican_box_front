45e0116dba2d397bac9823c6fc0dff7e05495a20
rega1237 <rega1237@gmail.com>
2023-12-19 12:08:51 -0400

    add user component top App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,51 @@ import SignIn from './pages/login/SignIn'
 import SignUp from './pages/login/SignUp'
 import Index from './pages/index/Index'
 import NavBar from './components/NavBar/NavBar'
+import { useNavigate } from 'react-router-dom';
 import UserContext from './components/store/userContext';
 import Subscription from './pages/subscription/Subscription';
+import UserAccount from './pages/user_account/UserAccount';
 
 function App() {
   const userCtx = useContext(UserContext);
-  const [isAuth, setIsAuth] = useState(false)
+  const [isLog, setIsLog] = useState(false)
+  const [isUserFetched, setIsUserFetched] = useState(false)
+
+  const fetchUser = async (id) => {
+    try{
+      await userCtx.getUser(id)
+      setIsUserFetched(true)
+    }catch(error){
+      userCtx.setError(true, "Error al cargar el usuario", false)
+      return error
+    }
+  }
 
   useEffect(() => {
     const id = localStorage.getItem('id')
     const isLogged = localStorage.getItem('isLogged')
 
     if (isLogged == 'true') {
-      userCtx.getUser(id);
+      fetchUser(id)
     }
   }, [])
 
   const handleInAuthView = () => {
-    setIsAuth(true)
+    setIsLog(true)
   }
 
   const handleOutAuthView = () => {
-    setIsAuth(false)
+    setIsLog(false)
   }
 
   return (
     <>
       <Router>
-        {!isAuth && <NavBar removeNavBar={handleInAuthView} addNavBar={handleOutAuthView} />}
+        {!isLog && <NavBar isUserFetched={isUserFetched} removeNavBar={handleInAuthView} addNavBar={handleOutAuthView} />}
         <Routes>
           <Route path="/signin" element={<SignIn addNavBar={handleOutAuthView} />} />
           <Route path="/signup" element={<SignUp addNavBar={handleOutAuthView} />} />
+          <Route path="/useraccount" element={<UserAccount removeNavBar={handleInAuthView} isUserFetched={isUserFetched} />} />
           <Route path="/" element={<Index />} />
           <Route path="/suscripciones" element={<Subscription />} />
         </Routes>
